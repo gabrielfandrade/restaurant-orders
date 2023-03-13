4c712614fe4e0eedf801d38e4c859d5ca8359001
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2023-03-13 12:25:56 -0300

    feat: Inventory Control

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -16,10 +16,32 @@ class InventoryControl:
     }
 
     def __init__(self):
-        pass
+        self.orders = list()
+
+        self.inventory = {
+            'pao': 0,
+            'carne': 0,
+            'queijo': 0,
+            'molho': 0,
+            'presunto': 0,
+            'massa': 0,
+            'frango': 0,
+        }
 
     def add_new_order(self, customer, order, day):
-        pass
+        new_order = [customer, order, day]
+
+        inventory = self.inventory
+
+        for ingredient in self.INGREDIENTS[order]:
+            if self.inventory[ingredient] < self.MINIMUM_INVENTORY[ingredient]:
+                inventory[ingredient] += 1
+            else:
+                return False
+
+        self.inventory = inventory
+
+        self.orders.append(new_order)
 
     def get_quantities_to_buy(self):
-        pass
+        return self.inventory
